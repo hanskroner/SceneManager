884d89c939f2ff71bd09b99618b6eee7be4354da
Hans Kröner <hans.kroner@gmail.com>
2022-11-06 15:06:13 +0100

    Add initial implementation of creating and deleting Group
    Creating a new Group should scroll it to the middle of the Sidebar and put it into rename mode. Ive tried with no
    success to have SwiftUI scroll the newly-created Group into view and some of the code that enables this is left
    commented here. Renaming is not possible yet. Deleting a Group should present a prompt, but I haven't figured
    out how SwiftUI handles that yet.
    
    The Swift compiler is having a really hard time crunching through the SwiftUI Views at this point and I'm not sure
    what I should be doing to simplify things for it. deCONZClientModel has been extracted into its own file and some
    actions pulled out to functions to at least keep compilation finishing - but it's taking quite a while for such a small
    project.

--- SceneManager/ContentView.swift
+++ SceneManager/ContentView.swift
@@ -23,34 +23,26 @@ struct ContentView: View {
                                     ForEach(children) { scene in
                                         NavigationLink(scene.name, value: scene)
                                             .contextMenu {
-                                                Button(action: { }, label: {
-                                                    Text("Rename Scene")
-                                                })
-                                                Button(action: { }, label: {
-                                                    Text("Delete Scene")
-                                                })
+                                                Button("Rename Scene") { }
+                                                Button("Delete Scene") { }
                                             }
                                     }
                                 } label: {
                                     Text(group.name)
                                         .contextMenu {
-                                            Button(action: { }, label: {
-                                                Text("Rename Group")
-                                            })
-                                            Button(action: { }, label: {
-                                                Text("Delete Group")
-                                            })
+                                            Button("Rename Group") { }
+                                            Button("Delete Group") {
+                                                deleteGroup(group: group)
+                                            }
                                         }
                                 }
                             } else {
                                 Text(group.name)
                                     .contextMenu {
-                                        Button(action: { }, label: {
-                                            Text("Rename Group")
-                                        })
-                                        Button(action: { }, label: {
-                                            Text("Delete Group")
-                                        })
+                                        Button("Rename Group") { }
+                                        Button("Delete Group") {
+                                            deleteGroup(group: group)
+                                        }
                                     }
                             }
                         }
@@ -61,20 +53,17 @@ struct ContentView: View {
                 VStack {
                     Divider()
                     HStack {
-                        Menu {
-                            Button(action: { }, label: {
-                                Text("New Group")
-                            })
-                            Button(action: { }, label: {
-                                Text("New Scene")
-                            })
-                        } label: {
+                        Button(action: {
+                            Task {
+                                await deconzModel.createNewGroup()
+                            }
+                        }) {
                             Label("", systemImage: "plus")
                         }
-                        .menuStyle(.borderlessButton)
-                        .menuIndicator(.hidden)
-                        .fixedSize()
-
+                        .buttonStyle(.plain)
+                        .font(.system(size: 14))
+                        .help("New Group")
+                        
                         Spacer()
                     }
                     .padding([.leading, .bottom], 8)
@@ -93,6 +82,13 @@ struct ContentView: View {
             }
         }
     }
+    
+    func deleteGroup(group: SidebarItem) {
+        Task {
+            guard let groupID = group.groupID else { return }
+            await deconzModel.deleteGroup(groupID: groupID)
+        }
+    }
 }
 
 struct SidebarItem: Identifiable, Hashable {
@@ -104,164 +100,6 @@ struct SidebarItem: Identifiable, Hashable {
     
     var groupID: Int?
     var sceneID: Int?
-}
-
-enum ModifySceneRange {
-    case allLightsInGroup
-    case selectedLightsOnly
-}
-
-class deCONZClientModel: ObservableObject {
-    private let deconzClient = deCONZClient()
-    
-    private let encoder = JSONEncoder()
-    
-    @Published var selectedSidebarItem: SidebarItem? = nil {
-        didSet {
-            if (oldValue != selectedSidebarItem) {
-                self.selectedSceneLights.removeAll()
-            }
-            
-            if (selectedSidebarItem == nil) {
-                self.sceneLights = [SceneLight]()
-            }
-            
-            if let groupID = selectedSidebarItem?.groupID, let sceneID = selectedSidebarItem?.sceneID {
-                Task {
-                    await self.updateSceneLights(forGroupID: groupID, sceneID: sceneID)
-                }
-            }
-        }
-    }
-    
-    @Published var selectedSceneLights = Set<SceneLight>()
-    
-    @Published var jsonStateText = ""
-    
-    @Published private(set) var sidebarItems = [SidebarItem]()
-    @Published private(set) var sceneLights = [SceneLight]()
-    
-    private var cacheLights: [Int: deCONZLight]?
-    private var cacheGroups: [Int: deCONZGroup]?
-    private var cacheScenes: [Int: [Int: deCONZScene]]?
-    
-    init() {
-        Task {
-            self.cacheLights = try await deconzClient.getAllLights()
-            (self.cacheGroups, self.cacheScenes) = try await deconzClient.getAllGroups()
-            
-            Task {
-                await refreshSidebarItems()
-            }
-        }
-    }
-    
-    private func refreshSidebarItems() async {
-        var updatedSidebarItems = [SidebarItem]()
-        
-        guard let cacheGroups = self.cacheGroups,
-              let cacheScenes = self.cacheScenes else { return }
-        
-        // Ignore Groups where 'devicemembership' is not empty
-        // These groups are created by switches or sensors and are not the kind we're looking for.
-        let filteredGroups = cacheGroups.filter({ $0.value.devicemembership?.isEmpty ?? true })
-        
-        for (_, group) in filteredGroups {
-            guard let groupName = group.name,
-                  let groupStringID = group.id,
-                  let groupID = Int(groupStringID),
-                  let scenes = group.scenes
-            else { return }
-            
-            var groupItem = SidebarItem(id: "G\(groupID)", name: groupName, groupID: groupID)
-            
-            for (sceneStringID) in scenes {
-                guard let sceneID = Int(sceneStringID),
-                      let sceneName = cacheScenes[groupID]?[sceneID]?.name
-                else { return }
-                
-                let sceneItem = SidebarItem(id: "G\(groupID)S\(sceneID)", name: sceneName, parentName: groupName, groupID: groupID, sceneID: sceneID)
-                
-                if (groupItem.children == nil) {
-                    groupItem.children = [SidebarItem]()
-                }
-                
-                groupItem.children!.append(sceneItem)
-            }
-            
-            // Sort Scene names alphabetically
-            groupItem.children = groupItem.children?.sorted(by: { $0.name.localizedStandardCompare($1.name) == .orderedAscending })
-            updatedSidebarItems.append(groupItem)
-        }
-        
-        // Sort Group names alphabetically
-        let list = updatedSidebarItems.sorted(by: { $0.name.localizedStandardCompare($1.name) == .orderedAscending })
-        await MainActor.run {
-            self.sidebarItems = list
-        }
-    }
-    
-    private func updateSceneLights(forGroupID groupID: Int, sceneID: Int) async {
-        var updatedSceneLights = [SceneLight]()
-        
-        let sceneAttributes = try? await deconzClient.getSceneAttributes(groupID: groupID, sceneID: sceneID)
-        
-        guard let cacheLights = self.cacheLights,
-              let cacheGroups = self.cacheGroups,
-              let sceneAttributes = sceneAttributes
-        else { return }
-        
-        encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
-        
-        for (stringLightID) in cacheGroups[groupID]?.lights ?? [] {
-            guard let lightID = Int(stringLightID),
-                  let light = cacheLights[lightID],
-                  let lightName = light.name,
-                  let lightState = sceneAttributes[lightID]
-            else { return }
-            
-            let stateString = lightState.prettyPrint
-            updatedSceneLights.append(SceneLight(id: "G\(groupID)S\(sceneID)L\(lightID)", lightID: lightID, name: lightName, state: stateString))
-        }
-        
-        // Sort Light names alphabetically
-        let list = updatedSceneLights.sorted(by: { $0.name.localizedStandardCompare($1.name) == .orderedAscending })
-        await MainActor.run {
-            self.sceneLights = list
-        }
-    }
-    
-    func modifyScene(range: ModifySceneRange) async {
-        guard let selectedSidebarItem = selectedSidebarItem,
-              let groupID = selectedSidebarItem.groupID,
-              let sceneID = selectedSidebarItem.sceneID
-        else { return }
-        
-        let lightIDs: [Int]
-        switch range {
-        case .allLightsInGroup:
-            lightIDs = self.sceneLights.map({ $0.lightID }).sorted()
-        case .selectedLightsOnly:
-            lightIDs = selectedSceneLights.map({ $0.lightID }).sorted()
-        }
-        
-        guard let _ = try? await deconzClient.modifyScene(groupID: groupID, sceneID: sceneID, lightIDs: lightIDs, state: jsonStateText) else {
-            // FIXME: Handle erros
-            print("Error Updating Group \(groupID), Scene \(sceneID), Lights \(lightIDs)")
-            return
-        }
-        
-        // If the request was successful, store the new JSON state in the modified lights
-        var sceneLightsCopy = self.sceneLights
-        for (lightID, _) in lightIDs.enumerated() {
-            sceneLightsCopy[lightID].state = jsonStateText
-        }
-
-        let list = sceneLightsCopy
-        await MainActor.run {
-            self.sceneLights = list
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
