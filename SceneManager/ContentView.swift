1ecfb21c9b440d3d638e46a778dc0cbfe00cbc7c
Hans Kröner <hans.kroner@gmail.com>
2022-11-06 09:29:05 +0100

    Move Settings View to its own files

--- SceneManager/ContentView.swift
+++ SceneManager/ContentView.swift
@@ -7,39 +7,6 @@
 
 import SwiftUI
 
-struct SettingsView: View {
-    @AppStorage("deconz_url") private var url = ""
-    @AppStorage("deconz_key") private var key = ""
-    
-    private enum Tabs: Hashable {
-        case deconz
-    }
-    
-    var body: some View {
-        TabView {
-            Grid(alignment: .trailing) {
-                GridRow {
-                    Spacer()
-                    Text("deCONZ URL:")
-                    TextField("http://127.0.0.1:8080", text: $url)
-                }
-                
-                GridRow {
-                    Spacer()
-                    Text("deCONZ API Key:")
-                    TextField("ABCDEF1234", text: $key)
-                }
-            }
-            .tabItem {
-                Label("deCONZ", systemImage: "network")
-            }
-            .tag(Tabs.deconz)
-        }
-        .padding()
-        .frame(width: 375, height: 100)
-    }
-}
-
 struct ContentView: View {
     @SceneStorage("inspector") private var showInspector = false
     
