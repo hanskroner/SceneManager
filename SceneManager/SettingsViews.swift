83bfcbbeb2773a11f7b5df76c4e359c77152e1c6
Hans Kröner <hans.kroner@gmail.com>
2022-11-06 09:29:05 +0100

    Move Settings View to its own files

---
+++ SceneManager/SettingsViews.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsView.swift
+//  SceneManager
+//
+//  Created by Hans Kröner on 06/11/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @AppStorage("deconz_url") private var url = ""
+    @AppStorage("deconz_key") private var key = ""
+    
+    private enum Tabs: Hashable {
+        case deconz
+    }
+    
+    var body: some View {
+        TabView {
+            Grid(alignment: .trailing) {
+                GridRow {
+                    Spacer()
+                    Text("deCONZ URL:")
+                    TextField("http://127.0.0.1:8080", text: $url)
+                }
+                
+                GridRow {
+                    Spacer()
+                    Text("deCONZ API Key:")
+                    TextField("ABCDEF1234", text: $key)
+                }
+            }
+            .tabItem {
+                Label("deCONZ", systemImage: "network")
+            }
+            .tag(Tabs.deconz)
+        }
+        .padding()
+        .frame(width: 375, height: 100)
+    }
+}
